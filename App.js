f639fa99e4ae8e79184310939de01d003db8f079
Akhilkota18 <20039345@mydbs.ie>
2024-07-09 00:07:39 +0100

    app frontend

--- App.js
+++ App.js
@@ -12,9 +12,31 @@ function App() {
     charging_time: '',
     price: ''
   });
-
+}
   useEffect(() => {
     axios.get('http://localhost:5000/ev')
       .then(response => setEvs(response.data))
       .catch(error => console.error('Error fetching data:', error));
   }, []);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:5000/ev', formData)
+      .then(response => {
+        setEvs([...evs, response.data]);
+        setFormData({
+          make: '',
+          model: '',
+          year: '',
+          battery_capacity: '',
+          range: '',
+          charging_time: '',
+          price: ''
+        });
+      })
+      .catch(error => console.error('Error adding EV:', error));
+  };
